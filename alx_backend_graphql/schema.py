3677f58ec0cfb3849c966f9104545f6f4d48eed5
mahakamal-e <mahakamal09@gmail.com>
2025-08-17 23:34:08 +0300

    edit schema file

--- alx_backend_graphql/schema.py
+++ alx_backend_graphql/schema.py
@@ -1,9 +1,33 @@
+#!/usr/bin/env python3
+"""
+GraphQL example using Graphene.
+
+This module defines a simple GraphQL schema with a single query field 'hello'.
+"""
+
 import graphene
 
-class Query(graphene.ObjectType):
-    hello = graphene.String()
 
-    def resolve_hello(root, info):
+class Query(graphene.ObjectType):
+    """
+    Root GraphQL query type.
+    
+    Fields:
+        hello (str): Returns a simple greeting message.
+    """
+    hello = graphene.String(description="Returns a greeting message")
+
+    def resolve_hello(self, info):
+        """
+        Resolver for the 'hello' field.
+
+        Args:
+            info: GraphQL execution info.
+
+        Returns:
+            str: A greeting message.
+        """
         return "Hello, GraphQL!"
+
 
 schema = graphene.Schema(query=Query)
